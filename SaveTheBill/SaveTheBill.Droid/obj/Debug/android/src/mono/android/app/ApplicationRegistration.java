e0d01b7cc493c40bbfde9c7d3ceaa7b4b13ad658
zmartl <luca.marti@bluewin.ch>
2016-12-02 16:37:57 +0100

    init commit

---
+++ SaveTheBill/SaveTheBill.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("SaveTheBill.Droid.MainApplication, SaveTheBill.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5550095bb60770f4634569ffb2c3cbbfc.MainApplication.class, md5550095bb60770f4634569ffb2c3cbbfc.MainApplication.__md_methods);
+		
+	}
+}
